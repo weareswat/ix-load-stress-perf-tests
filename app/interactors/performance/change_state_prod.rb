e75da255bb72159b1e96fa1f033e5bb5683919c0
ruisalexsantos <rui.santos@rupeal.com>
2020-11-23 16:01:30 +0000

    Adapt LSP concurrent scenario to invoices https://www.pivotaltracker.com/story/show/175821626 What's new: - Added scenario to send test invoices.

--- app/interactors/performance/change_state_prod.rb
+++ app/interactors/performance/change_state_prod.rb
@@ -4,32 +4,32 @@ module Performance
     def self.run_tests(params)
       accounts = params[:accounts]
       num_docs = params[:num_docs] || 1
-
-      accounts.each do | account |
+      env = params[:environment]
+      accounts.each do |account|
         Concurrent::Promise.execute do
-          perform_change_state(account[:subdomain], account[:api_key], num_docs, account[:sequence_id])
+          perform_change_state(account[:subdomain], account[:api_key], env, num_docs, account[:sequence_id], account[:doc_type])
         end
       end
     end
 
     private
 
-    def self.perform_change_state(subdomain, api_key, num_docs, sequence_id)
+    def self.perform_change_state(subdomain, api_key, env, num_docs, sequence_id, doc_type)
 
-      invoices = create_documents(subdomain, api_key, num_docs, sequence_id)
+      invoices = create_documents(subdomain, api_key, env, num_docs, sequence_id, doc_type)
 
-      change_state_body = { "invoice_receipt": {"state": "finalized"} }
+      change_state_body = {"#{doc_type}": {"state": "finalized"}}
+      invoices.each do |invoice_id|
 
-      invoices.each do | invoice_id |
         # call PUT /api/v3/invoice_receipts/:document-id/change-state.json
-        rest_endpoint = change_state_endpoint(subdomain, api_key, 'production', invoice_id)
+        rest_endpoint = change_state_endpoint(subdomain, api_key, env, invoice_id, doc_type)
 
-        Rails.logger.info "\nCalling #{rest_endpoint}"
+        Rails.logger.info "\nCalling #{rest_endpoint} body: #{change_state_body}"
         starting = Process.clock_gettime(Process::CLOCK_MONOTONIC)
         response = ::RestClient::Request.execute(:method => :put,
                                                  :url => rest_endpoint,
                                                  :payload => change_state_body,
-                                                 :headers => { :content_type => "application/json" },
+                                                 :headers => {:content_type => "application/json"},
                                                  :timeout => 16,
                                                  :open_timeout => 16)
         ending = Process.clock_gettime(Process::CLOCK_MONOTONIC)
@@ -55,15 +55,15 @@ module Performance
 
     private
 
-    def self.create_documents(subdomain = 'bodesvermelhos', api_key = '5312d0f8f4a20479eb8ae1f44fc973086da07978', num_docs = 1,
-                       sequence_id)
+    def self.create_documents(subdomain = 'bodesvermelhos', api_key = '5312d0f8f4a20479eb8ae1f44fc973086da07978', env = 'production', num_docs = 1,
+                              sequence_id, doc_type)
       ids = []
 
       num_docs.times do
         proprietary_uid = generate_proprietary_uid
         date = Date.today.strftime('%Y-%m-%d')
         create_body = {
-            "invoice_receipt": {
+            "#{doc_type}": {
                 "date": date,
                 "due_date": date,
                 "reference": proprietary_uid,
@@ -77,22 +77,22 @@ module Performance
                 },
                 "items": [
                     {"name": "Artigo", "description": "Produto X",
-                     "unit_price": "13.1132","quantity": "1","tax": {"name": "IVA23"}, "unit": "service","discount": "0"}
+                     "unit_price": "13.1132", "quantity": "1", "tax": {"name": "IVA23"}, "unit": "service", "discount": "0"}
                 ]
             },
             "proprietary_uid": proprietary_uid
         }
 
-        rest_endpoint = create_endpoint(subdomain, api_key)
+        rest_endpoint = create_endpoint(subdomain, api_key, doc_type, env)
         puts "\nCreating document: #{rest_endpoint}"
         response = ::RestClient::Request.execute(:method => :post,
                                                  :url => rest_endpoint,
                                                  :payload => create_body,
-                                                 :headers => { :content_type => "application/json" },
+                                                 :headers => {:content_type => "application/json"},
                                                  :timeout => 16,
                                                  :open_timeout => 16)
 
-        response_id = JSON.parse(response)["invoice_receipt"]["id"]
+        response_id = JSON.parse(response)["#{doc_type}"]["id"]
         ids << response_id
 
         puts "Document created (#{subdomain}):  #{response}"
@@ -113,23 +113,23 @@ module Performance
       ids
     end
 
-    def self.create_endpoint(subdomain, api_key, env = 'production')
+    def self.create_endpoint(subdomain, api_key, doc_type, env = 'production')
       if env == 'staging'
-        "http://#{subdomain}.obiwan.rupeal.com/invoice_receipts.json?api_key=#{api_key}"
+        "http://#{subdomain}.obiwan.rupeal.com/i#{doc_type}s.json?api_key=#{api_key}"
       elsif env == 'development'
-        "http://localhost:3001/invoice_receipts.json?api_key=#{api_key}"
+        "http://localhost:3001/#{doc_type}s.json?api_key=#{api_key}"
       elsif env == 'production'
-        "https://#{subdomain}.app.invoicexpress.com/invoice_receipts.json?api_key=#{api_key}"
+        "https://#{subdomain}.app.invoicexpress.com/#{doc_type}s.json?api_key=#{api_key}"
       end
     end
 
-    def self.change_state_endpoint(subdomain, api_key, env = 'production', invoice_id)
+    def self.change_state_endpoint(subdomain, api_key, env = 'production', invoice_id, doc_type)
       if env == 'staging'
-        "http://#{subdomain}.obiwan.rupeal.com/invoice_receipts/#{invoice_id}/change-state.json?api_key=#{api_key}"
+        "http://#{subdomain}.obiwan.rupeal.com/#{doc_type}s/#{invoice_id}/change-state.json?api_key=#{api_key}"
       elsif env == 'development'
-        "http://localhost:3001/invoice_receipts.json?api_key=#{api_key}"
+        "http://localhost:3001/#{doc_type}/#{invoice_id}s/change-state.json?api_key=#{api_key}"
       elsif env == 'production'
-        "https://#{subdomain}.app.invoicexpress.com/invoice_receipts/#{invoice_id}/change-state.json?api_key=#{api_key}"
+        "https://#{subdomain}.app.invoicexpress.com/#{doc_type}s/#{invoice_id}/change-state.json?api_key=#{api_key}"
       end
     end
 
